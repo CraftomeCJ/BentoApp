c5c18962f9076fae3772bf9ef952b988a3e0bc5d
javier ho <tech@craftome.com>
2022-05-06 18:58:40 +0800

    bug filled animated splashedscreen to resolved

--- App.tsx
+++ App.tsx
@@ -1,4 +1,38 @@
+//default component`
 import React, { useState, useEffect } from 'react';
+//importing loader
 import PacmanLoader from 'react-spinners/PacmanLoader';
 import ClockLoader from 'react-spinners/ClockLoader';
 import './App.css'
+
+const App = () => {
+
+  //loading state
+  const [isloading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+
+    //wait for 5 seconds
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+  }, []);
+
+  //custom css for loader
+  const override = `
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+  `;
+
+  return isloading ?
+
+  //If page is still loading then splash screen
+  <PacmanLoader color={ '#36D7B7' } isLoading ={ isloading }css={ override } size={150} /> :
+  <h1 className='App'>
+    This is Splash Screen Page
+    {<ClockLoader color={ '#36D7B7' } isLoading={ isloading } css={ override } size={150} />}
+    </h1>
+  }
+
+  export default App;
